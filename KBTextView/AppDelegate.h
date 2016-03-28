709e90c5bc61d90e33a68b9f72ee8d7542ccfb9a
Mossion <liangtian@localhost>
2016-03-28 20:23:51 +0800

    Initial Commit

---
+++ KBTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KBTextView
+//
+//  Created by 梁添 on 16/3/28.
+//  Copyright © 2016年 梁添. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KBTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KBTextView
+//
+//  Created by 梁添 on 16/3/28.
+//  Copyright © 2016年 梁添. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
